eb15067bd5e4648ca6a2cd30883c9f545e0e3ff8
VikasSutar1009 <vikassutarfsd1020@gmail.com>
2026-02-27 19:59:13 +0530

    Initial react project

---
+++ src/components/ui/Table.jsx
@@ -0,0 +1,48 @@
+import "../../styles/table.css";
+
+const Table = () => {
+    return(
+        <div className="container">
+            <h3>Users Table</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Table;
